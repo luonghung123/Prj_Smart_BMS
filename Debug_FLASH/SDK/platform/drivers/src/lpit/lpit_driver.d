22c212039a50227971b0727d23edef626d32af7e
luonghung123 <160730685+luonghung123@users.noreply.github.com>
2026-03-22 15:58:04 +0700

    Initial commit

---
+++ Debug_FLASH/SDK/platform/drivers/src/lpit/lpit_driver.d
@@ -0,0 +1,38 @@
+SDK/platform/drivers/src/lpit/lpit_driver.o: \
+ D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/platform/drivers/src/lpit/lpit_driver.c \
+ D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/platform/drivers/inc/lpit_driver.h \
+ D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/platform/devices/status.h \
+ D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/platform/drivers/src/lpit/lpit_hw_access.h \
+ D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/platform/devices/device_registers.h \
+ D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/platform/devices/common/s32_core_cm4.h \
+ D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/platform/devices/S32K144/include/S32K144.h \
+ D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/platform/devices/S32K144/include/S32K144_features.h \
+ D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/platform/devices/devassert.h \
+ D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/platform/drivers/inc/interrupt_manager.h \
+ D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/platform/drivers/inc/clock_manager.h \
+ D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/platform/drivers/inc/clock.h \
+ d:\s32\s32_2_2\s32ds\software\s32sdk_s32k1xx_rtm_3.0.0\platform\drivers\src\clock\s32k1xx\clock_s32k1xx.h
+
+D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/platform/drivers/inc/lpit_driver.h:
+
+D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/platform/devices/status.h:
+
+D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/platform/drivers/src/lpit/lpit_hw_access.h:
+
+D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/platform/devices/device_registers.h:
+
+D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/platform/devices/common/s32_core_cm4.h:
+
+D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/platform/devices/S32K144/include/S32K144.h:
+
+D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/platform/devices/S32K144/include/S32K144_features.h:
+
+D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/platform/devices/devassert.h:
+
+D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/platform/drivers/inc/interrupt_manager.h:
+
+D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/platform/drivers/inc/clock_manager.h:
+
+D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/platform/drivers/inc/clock.h:
+
+d:\s32\s32_2_2\s32ds\software\s32sdk_s32k1xx_rtm_3.0.0\platform\drivers\src\clock\s32k1xx\clock_s32k1xx.h:
---
+++ Debug_FLASH/Sources/main.d
@@ -0,0 +1,118 @@
+Sources/main.o: ../Sources/main.c \
+ D:/S32/s32_2_2/SMART_CAN/Generated_Code/Cpu.h \
+ D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/platform/devices/device_registers.h \
+ D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/platform/devices/common/s32_core_cm4.h \
+ D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/platform/devices/S32K144/include/S32K144.h \
+ D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/platform/devices/S32K144/include/S32K144_features.h \
+ D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/platform/devices/devassert.h \
+ D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/platform/drivers/inc/interrupt_manager.h \
+ D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/platform/drivers/inc/clock.h \
+ D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/platform/devices/status.h \
+ d:\s32\s32_2_2\s32ds\software\s32sdk_s32k1xx_rtm_3.0.0\platform\drivers\src\clock\s32k1xx\clock_s32k1xx.h \
+ D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/rtos/osif/osif.h \
+ D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/platform/drivers/inc/csec_driver.h \
+ D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/platform/drivers/inc/interrupt_manager.h \
+ D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/platform/devices/callbacks.h \
+ D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/platform/drivers/inc/edma_driver.h \
+ D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/platform/drivers/inc/flexcan_driver.h \
+ D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/platform/drivers/inc/edma_driver.h \
+ D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/platform/drivers/inc/lpuart_driver.h \
+ D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/platform/drivers/inc/lpi2c_driver.h \
+ D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/platform/drivers/inc/adc_driver.h \
+ D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/platform/drivers/inc/lpit_driver.h \
+ D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/platform/devices/S32K144/startup/system_S32K144.h \
+ D:/S32/s32_2_2/SMART_CAN/Generated_Code/pin_mux.h \
+ D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/platform/drivers/inc/pins_driver.h \
+ D:/S32/s32_2_2/SMART_CAN/Generated_Code/clockMan1.h \
+ D:/S32/s32_2_2/SMART_CAN/Generated_Code/Cpu.h \
+ D:/S32/s32_2_2/SMART_CAN/Generated_Code/csec1.h \
+ D:/S32/s32_2_2/SMART_CAN/Generated_Code/dmaController1.h \
+ D:/S32/s32_2_2/SMART_CAN/Generated_Code/canCom1.h \
+ D:/S32/s32_2_2/SMART_CAN/Generated_Code/lpuart1.h \
+ D:/S32/s32_2_2/SMART_CAN/Generated_Code/lpi2c1.h \
+ D:/S32/s32_2_2/SMART_CAN/Generated_Code/adConv1.h \
+ D:/S32/s32_2_2/SMART_CAN/Generated_Code/lpit1.h \
+ D:/S32/s32_2_2/SMART_CAN/Generated_Code/clockMan1.h \
+ D:/S32/s32_2_2/SMART_CAN/Generated_Code/pin_mux.h \
+ D:/S32/s32_2_2/SMART_CAN/Generated_Code/lpit1.h \
+ D:/S32/s32_2_2/SMART_CAN/Generated_Code/adConv1.h \
+ D:/S32/s32_2_2/SMART_CAN/Generated_Code/lpi2c1.h \
+ D:/S32/s32_2_2/SMART_CAN/Generated_Code/canCom1.h
+
+D:/S32/s32_2_2/SMART_CAN/Generated_Code/Cpu.h:
+
+D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/platform/devices/device_registers.h:
+
+D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/platform/devices/common/s32_core_cm4.h:
+
+D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/platform/devices/S32K144/include/S32K144.h:
+
+D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/platform/devices/S32K144/include/S32K144_features.h:
+
+D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/platform/devices/devassert.h:
+
+D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/platform/drivers/inc/interrupt_manager.h:
+
+D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/platform/drivers/inc/clock.h:
+
+D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/platform/devices/status.h:
+
+d:\s32\s32_2_2\s32ds\software\s32sdk_s32k1xx_rtm_3.0.0\platform\drivers\src\clock\s32k1xx\clock_s32k1xx.h:
+
+D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/rtos/osif/osif.h:
+
+D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/platform/drivers/inc/csec_driver.h:
+
+D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/platform/drivers/inc/interrupt_manager.h:
+
+D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/platform/devices/callbacks.h:
+
+D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/platform/drivers/inc/edma_driver.h:
+
+D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/platform/drivers/inc/flexcan_driver.h:
+
+D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/platform/drivers/inc/edma_driver.h:
+
+D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/platform/drivers/inc/lpuart_driver.h:
+
+D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/platform/drivers/inc/lpi2c_driver.h:
+
+D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/platform/drivers/inc/adc_driver.h:
+
+D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/platform/drivers/inc/lpit_driver.h:
+
+D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/platform/devices/S32K144/startup/system_S32K144.h:
+
+D:/S32/s32_2_2/SMART_CAN/Generated_Code/pin_mux.h:
+
+D:/S32/s32_2_2/S32DS/software/S32SDK_S32K1xx_RTM_3.0.0/platform/drivers/inc/pins_driver.h:
+
+D:/S32/s32_2_2/SMART_CAN/Generated_Code/clockMan1.h:
+
+D:/S32/s32_2_2/SMART_CAN/Generated_Code/Cpu.h:
+
+D:/S32/s32_2_2/SMART_CAN/Generated_Code/csec1.h:
+
+D:/S32/s32_2_2/SMART_CAN/Generated_Code/dmaController1.h:
+
+D:/S32/s32_2_2/SMART_CAN/Generated_Code/canCom1.h:
+
+D:/S32/s32_2_2/SMART_CAN/Generated_Code/lpuart1.h:
+
+D:/S32/s32_2_2/SMART_CAN/Generated_Code/lpi2c1.h:
+
+D:/S32/s32_2_2/SMART_CAN/Generated_Code/adConv1.h:
+
+D:/S32/s32_2_2/SMART_CAN/Generated_Code/lpit1.h:
+
+D:/S32/s32_2_2/SMART_CAN/Generated_Code/clockMan1.h:
+
+D:/S32/s32_2_2/SMART_CAN/Generated_Code/pin_mux.h:
+
+D:/S32/s32_2_2/SMART_CAN/Generated_Code/lpit1.h:
+
+D:/S32/s32_2_2/SMART_CAN/Generated_Code/adConv1.h:
+
+D:/S32/s32_2_2/SMART_CAN/Generated_Code/lpi2c1.h:
+
+D:/S32/s32_2_2/SMART_CAN/Generated_Code/canCom1.h:
